6e5133d84cbe1880e5b11ad3d73024f1fc2ad74c
BryllieBrinkley <49615544+BryllieBrinkley@users.noreply.github.com>
2020-09-21 14:12:24 -0400

    Update vacation_chat_bot.py
    added comments and while loop at the end

--- vacation_chat_bot.py
+++ vacation_chat_bot.py
@@ -1,32 +1,42 @@
-import random
+from random import choice
 
-#Instrutions for User
-print("Welcome to the Vacation Planner Chat Bot!")
-print("Please put in your favorite weather \n(sunny, rainy, cloudy, or windy) and the bot will \n generate the vaction of a lifetime!")
+#def get_bot_response
+#user_response = input("Please enter your favorite weather: ")
 
-user_response = input("Please enter your favorite weather: ")
-
+#These are lists of places and one will be randomly called from a list when the user enters his/her corresponding favorite weather
 rainy_places = ["Mawsynram, Meghalaya, India", "Cropp River, New Zealand", "Sinchaun Province, China", " Haleakala Volcano National Park"]
 sunny_places = ["Phuket, Thailand", "Havana, Cuba", "Rio de Janeiro, Brazil", "Costa Del Sol, Spain"]
 cloudy_places = ["Seattle, Washington", "Torshavn, Faroe Islands", "Dikson, Russia", "Sao Joaquim, Brazil"]
 windy_places = ["Patagonia","Amarillo, Texas", "Mount Everest", "Barrow, Alaska"]
 
 
+# loop displays bot response w vacation suggestion
 def favorite_weather(user_respone):
   
   if user_respone == "rainy":
-    print("You should visit " + random.choice(rainy_places) + ".")
+    return print("You should visit " + choice(rainy_places) + ".")
     
   elif user_respone == "sunny":
-    print("You should visit " + random.choice(sunny_places) + ".")
+    return print("You should visit " + choice(sunny_places) + ".")
     
   elif user_respone == "cloudy":
-    print("You should visit " + random.choice(cloudy_places) + ".")
+    return print("You should visit " + choice(cloudy_places) + ".")
     
   elif user_respone == "windy":
-    print("You should visit " + random.choice(windy_places) + ".")
+    return print("You should visit " + choice(windy_places) + ".")
     
   else:
-    print("Sorry I don't have a suggestion for a vacation.")
+    return print("Sorry I don't have a suggestion for a vacation.")
 
-favorite_weather(user_response)
+#Instrutions for User / Topic/ Greeting
+print("Welcome to the Vacation Planner Chat Bot!")
+print("Please put in your favorite weather \n(sunny, rainy, cloudy, or windy) and the bot will \n generate the vaction of a lifetime!")
+
+#TODO: we want to keep repeating until the user enters "done" what should we put here?
+while True:
+  user_response = input("What is your favorite weather?: ")
+# Quits program when user responds with 'done'
+  if user_response == 'done':
+    break
+    bot_response = favorite_weather(user_response)
+  favorite_weather(user_response)
